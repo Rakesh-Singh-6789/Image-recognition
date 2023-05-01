ef66c19a5ab22818f695b1006f530aa2bbb87759
root <root@ip-172-31-93-136.ec2.internal>
2023-05-01 19:47:16 +0000

    frontend added

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {Button,Card} from 'react-bootstrap';
+import Pick from './Components/Pick';
+function App() {
+  return (
+    <div  className="App">
+      <Pick/>
+     
+    </div>
+  );
+}
+
+export default App;
